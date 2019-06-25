60cd96a7e5c067fe1939fb64beacfd00d97c101e
web1_okhrimchuk_d <web1@marat.ua>
2019-06-25 15:55:39 +0300

    add request for business account

--- payment_method_interkassa/module_info.php
+++ payment_method_interkassa/module_info.php
@@ -9,7 +9,7 @@ $com_info = [
              'window_type' => 'xhr', // Load method. Possible values xhr/iframe
              'w'           => 600, // Window width
              'h'           => 550, // Window height
-             'version'     => '1.0', // Module version
+             'version'     => '1.1', // Module version
              'author'      => 'interkassa.com', // Author info
              'icon_class'  => 'icon-barcode',
             ];
